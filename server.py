ffcadcf818ec58572d44bfd8574fdfddfa773b51
Alex Pimania <randomiseme33@gmail.com>
2017-05-13 14:36:48 +1000

    Update server.py

--- server.py
+++ server.py
@@ -9,16 +9,16 @@ class emailHandler(tornado.web.RequestHandler):
         with open("emailLog.txt", "a") as emailFile:
             emailFile.write(email + "\n")
             
-        sender = 'snazzyemailsender@gmail.com'
-        receivers = ['programmelearn@gmail.com', 'randomiseme33@gmail.com']
+        sender = 'a@b.com'
+        receivers = ['a@b.com', 'a@b.com']
 
-        userMessage = "reply-to: programmelearn@gmail.com, randomiseme33@gmail.com\nSubject: Thank you for using http://academical.ml\n\nNow you can send us feedbacks through this email.\n:)"
+        userMessage = "reply-to: a@b.com, a@b.com\nSubject: Thank you for using http://academical.ml\n\nNow you can send us feedbacks through this email.\n:)"
 
         adminMessage = "reply-to:" + email + "\nSubject: New Email Submission\n\nSomeone has submitted their email address!!\nThe email address was: " + email + "\nReply to this email to send message to user."
 
         try:
            smtpObj = smtplib.SMTP_SSL('smtp.gmail.com:465')
-           smtpObj.login("snazzyemailsender@gmail.com", "wsxcdeWSXCDE99!")
+           smtpObj.login("a@b.com", "pswd")
            smtpObj.sendmail(sender, receivers, adminMessage)         
         except SMTPException:
            print "Error: unable to send email"
@@ -26,7 +26,7 @@ class emailHandler(tornado.web.RequestHandler):
            smtpObj.quit()
         try:
            smtpObj = smtplib.SMTP_SSL('smtp.gmail.com:465')
-           smtpObj.login("snazzyemailsender@gmail.com", "wsxcdeWSXCDE99!")
+           smtpObj.login("a@b.com", "")
            smtpObj.sendmail(sender, email, userMessage)    
         except SMTPException:
            print "Error: unable to send email"
